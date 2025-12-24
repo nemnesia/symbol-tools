0d83ae5d67b077d45601ffa836d20f05548da2bc
ccHarvestasya <harvestasya84@gmail.com>
2025-12-25 04:58:44 +0900

    chore: .gitignoreの整理とサブモジュールのパス修正、モデルコピー用スクリプトのパスを更新

--- packages/symbol-catbuffer/scripts/copy_models.sh
+++ packages/symbol-catbuffer/scripts/copy_models.sh
@@ -9,18 +9,18 @@ rm -rf ./src/symbol
 mkdir -p ./src/symbol
 mkdir -p ./src/utils
 
-cp ./_symbol/sdk/javascript/src/symbol/models.js ./src/symbol
-cp ./_symbol/sdk/javascript/src/BaseValue.js ./src
-cp ./_symbol/sdk/javascript/src/ByteArray.js ./src
-cp ./_symbol/sdk/javascript/src/utils/BufferView.js ./src/utils
-cp ./_symbol/sdk/javascript/src/utils/Writer.js ./src/utils
-cp ./_symbol/sdk/javascript/src/utils/arrayHelpers.js ./src/utils
-cp ./_symbol/sdk/javascript/src/utils/converter.js ./src/utils
-cp ./_symbol/sdk/javascript/src/utils/charMapping.js ./src/utils
+cp ../../_symbol/sdk/javascript/src/symbol/models.js ./src/symbol
+cp ../../_symbol/sdk/javascript/src/BaseValue.js ./src
+cp ../../_symbol/sdk/javascript/src/ByteArray.js ./src
+cp ../../_symbol/sdk/javascript/src/utils/BufferView.js ./src/utils
+cp ../../_symbol/sdk/javascript/src/utils/Writer.js ./src/utils
+cp ../../_symbol/sdk/javascript/src/utils/arrayHelpers.js ./src/utils
+cp ../../_symbol/sdk/javascript/src/utils/converter.js ./src/utils
+cp ../../_symbol/sdk/javascript/src/utils/charMapping.js ./src/utils
 
-cp ./_symbol/sdk/javascript/src/symbol/models_ts.js ./src/symbol
-cp ./_symbol/sdk/javascript/src/symbol/Network.js ./src/symbol
-cp ./_symbol/sdk/javascript/src/CryptoTypes.js ./src
+cp ../../_symbol/sdk/javascript/src/symbol/models_ts.js ./src/symbol
+cp ../../_symbol/sdk/javascript/src/symbol/Network.js ./src/symbol
+cp ../../_symbol/sdk/javascript/src/CryptoTypes.js ./src
 
 node scripts/generate-index.js
 
