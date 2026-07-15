e64ad74f6b7ee350e8d7df5cbd1bdb5c826eeaf7
ccHarvestasya <harvestasya84@gmail.com>
2026-07-16 06:09:53 +0900

    fix(symbol-sdk): 不要な --ignoreConfig オプションを削除

--- packages/symbol-sdk/scripts/create_types.sh
+++ packages/symbol-sdk/scripts/create_types.sh
@@ -7,6 +7,6 @@ cd "$(dirname "$0")/.."
 rm -rf ./types
 mkdir -p ./types
 
-npx tsc --ignoreConfig --allowJs --declaration --emitDeclarationOnly --outDir ./types ./src/index.js
-npx tsc --ignoreConfig --allowJs --declaration --emitDeclarationOnly --outDir ./types ./src/symbol/index.js
-npx tsc --ignoreConfig --allowJs --declaration --emitDeclarationOnly --outDir ./types ./src/nem/index.js
+npx tsc --allowJs --declaration --emitDeclarationOnly --outDir ./types ./src/index.js
+npx tsc --allowJs --declaration --emitDeclarationOnly --outDir ./types ./src/symbol/index.js
+npx tsc --allowJs --declaration --emitDeclarationOnly --outDir ./types ./src/nem/index.js
